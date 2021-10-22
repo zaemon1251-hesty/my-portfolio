fdb24ca873a23c43f5dca88ce3c494f59b4bc676
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-10-22 19:22:56 +0900

    fix .js to .ts*

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import React from 'react'
+import { AppProps } from 'next/app';
+
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Component {...pageProps} />
+  );
+};
+
+export default MyApp
