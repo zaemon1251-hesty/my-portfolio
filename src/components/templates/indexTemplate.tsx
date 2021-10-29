90367d9439d0536171dac85423f4c539bf84e877
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-10-30 02:12:51 +0900

    fix index template

--- src/components/templates/indexTemplate.tsx
+++ src/components/templates/indexTemplate.tsx
@@ -1,9 +1,17 @@
+import { MyHeader } from "../organisms/MyHeader"
+import { ProfileCard } from "../organisms/profileCard"
+
 type Props = {
 }
 
 
 export const indexTemplate:React.VFC<Props> = () => {
     return (
-        <h1></h1>
-    )
-}
+        <div>
+            <MyHeader />
+            <main className="max-w-3xl px-4 mx-auto my-10 sm:px-6 xl:max-w-5xl xl:px-0">
+                <ProfileCard />
+            </main>
+        </div>
+    );
+};
