7a17636fd55059ed1892cee8a1934dc127142533
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-10-30 20:12:02 +0900

    re: fix pages

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,1 +1,26 @@
-export default {}
+import { GetStaticProps, NextPage } from "next";
+import { BlogPostsTemplate } from "../../components/templates/BlogPostsTemplate";
+import { BlogPost } from "../../types";
+import { BlogPostsSample } from "../../utils/sample";
+
+type StaticProps = {
+    blogPosts:BlogPost[]
+}
+
+export const BlogPostsPage:NextPage<StaticProps> = ({
+    blogPosts
+}) =>{
+    return (
+        <BlogPostsTemplate blogPosts={blogPosts} />
+    );
+};
+
+export const getStaticProps:GetStaticProps<StaticProps> = (context) => {
+    const props:StaticProps = {
+        blogPosts:BlogPostsSample
+    }
+    return { props }
+}
+
+
+export default BlogPostsPage;
--- src/pages/blog/post/[id].tsx
+++ src/pages/blog/post/[id].tsx
@@ -1,1 +1,30 @@
-export default {}
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { BlogIndexTemplate } from "../../../components/templates/BlogIndexTemplate";
+import { BlogPost } from "../../../types";
+import { BlogPostSample, BlogPostsSample } from "../../../utils/sample";
+
+export const BlogIndexPage:NextPage<BlogPost> = ({
+    id,
+    title,
+    eyecatch,
+    description,
+    detail,
+    date,
+}) => {
+    return (
+        <BlogIndexTemplate title={title} eyecatch={eyecatch} detail={detail} date={date} />
+    );
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+    const paths = BlogPostsSample.map((BlogPost) => `/blog/${BlogPost.id}`)
+    return { paths, fallback:false }
+}
+
+export const getStaticProps: GetStaticProps<BlogPost> = (context) => {
+    const id = context.params?.id
+    const props = BlogPostSample
+    return { props };
+}
+
+export default BlogIndexPage;
