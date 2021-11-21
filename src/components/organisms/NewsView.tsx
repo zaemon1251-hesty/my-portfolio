ab535aca8f3df2636209f421c620eac40dac9103
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-11-22 00:20:10 +0900

    chore

--- src/components/organisms/NewsView.tsx
+++ src/components/organisms/NewsView.tsx
@@ -22,6 +22,6 @@ export const NewsView: React.VFC<Props> = ({
                 <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
             </div>
         </section>
-  );
+    );
 };
 
