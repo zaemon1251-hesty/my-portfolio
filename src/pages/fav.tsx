dc127ffa32f415f9fe96514ee6d60601b7e6dd09
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-11-23 17:53:31 +0900

    add fav page

--- src/pages/fav.tsx
+++ src/pages/fav.tsx
@@ -1,8 +1,8 @@
 import { GetStaticProps, NextPage } from "next";
 import { FavTemplate } from "../components/templates/FavTemplate";
 
-export const IndexPage: NextPage = () => {
+export const FavePage: NextPage = () => {
   return <FavTemplate />;
 };
 
-export default IndexPage;
+export default FavePage;
