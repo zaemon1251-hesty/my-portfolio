b3f32bfd52bdc209866f2d9248ebd66f10548530
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-11-23 17:52:53 +0900

    chore

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ module.exports = {
   images: {
     domains: [
       'tiny-images-mori.s3.ap-northeast-1.amazonaws.com', 
-      'zaemon1251-hesty.herokuapp.com'
+      'zaemon1251-hesty.herokuapp.com',
+      'pbs.twimg.com'
     ],
+  },
+  env: {
+    CMS_URL:"https://zaemon1251-hesty.herokuapp.com/api",
+    NEWS_URL:"https://newsapi.org/v2/top-headlines",
+    TWITTER_URL:"https://api.twitter.com",
+    REACT_APP_API_ENDPOINT_URL:"https://dmxjf92174.execute-api.ap-northeast-1.amazonaws.com/production"
   },
 }
