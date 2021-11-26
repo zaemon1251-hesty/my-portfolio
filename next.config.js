6277b3d0e655eff25082083ad6df923895cd353e
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-11-26 09:43:34 +0900

    add: env

--- next.config.js
+++ next.config.js
@@ -11,5 +11,6 @@ module.exports = {
     CMS_URL:"https://zaemon1251-hesty.herokuapp.com/api",
     NEWS_URL:"https://newsapi.org/v2/top-headlines",
     TWITTER_URL:"https://api.twitter.com",
+    REACT_APP_API_ENDPOINT_URL:"https://dmxjf92174.execute-api.ap-northeast-1.amazonaws.com/production"
   },
 }
