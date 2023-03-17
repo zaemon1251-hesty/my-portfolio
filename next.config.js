c2af33f53389950d4084c94e68446abd57e12f12
zaemon1251-hesty <hisakawa1251@gmail.com>
2023-03-17 22:15:03 +0900

    fix: migrate from heroku to render

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ module.exports = {
   images: {
     domains: [
       'tiny-images-mori.s3.ap-northeast-1.amazonaws.com', 
-      'zaemon1251-hesty.herokuapp.com',
+      'portfolio-backend-lqkw.onrender.com',
       'pbs.twimg.com'
     ],
   },
   env: {
-    CMS_URL:"https://zaemon1251-hesty.herokuapp.com/api",
+    CMS_URL:"https://portfolio-backend-lqkw.onrender.com/api",
     NEWS_URL:"https://newsapi.org/v2/top-headlines",
     TWITTER_URL:"https://api.twitter.com",
     REACT_APP_API_ENDPOINT_URL:"https://dmxjf92174.execute-api.ap-northeast-1.amazonaws.com/production"
